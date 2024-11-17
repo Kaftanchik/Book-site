689dd2850fd9bc478a031241cf2be0b25b36158a
Володимир Кафтан <118433540+Kaftanchik@users.noreply.github.com>
2024-11-17 21:40:48 +0200

    Add files via upload

---
+++ catalog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Author  )
+admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Publisher)
+admin.site.register(Status)
+admin.site.register(BookInstance)
+
